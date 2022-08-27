13329c31628de6bf006769013df61e3713e91701
Christian Bergane <107763472+CBergane@users.noreply.github.com>
2022-08-27 19:50:20 +0200

    changed game loop with fewer lines of code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,16 @@
 const option = document.querySelectorAll(".option");
+const allOptions = ['Rock', 'Paper', 'Scissors', 'Spock', 'Lizard'];
 let playerScore = 0;
 let cpuScore = 0;
 
 // add event listener on click for buttons options
 option.forEach((option) => {
     option.addEventListener('click', function () {
-        const playerInput = this.value;
+        
 
         // Games option in a array and a loop to make a random choice
-        const cpuOptions = ['Rock', 'Paper', 'Scissors', 'Lizard', 'Spock'];
-        const cpuInput = cpuOptions[Math.floor(Math.random() * 5)];
+        const playerInput = allOptions.indexOf(this.value);
+        const cpuInput = Math.floor(Math.random() * allOptions.length);
 
         inspectInput(playerInput, cpuInput);
         updateScore();
@@ -23,81 +24,25 @@ option.forEach((option) => {
 
 // Checking current game
 function inspectInput(playerInput, cpuInput) {
-    const currentGame = `${playerInput} vs ${cpuInput}`;
-
-    // Check if its a tie
-    if (playerInput === cpuInput) {
-        alert(`${currentGame} is a tie!!`);
-        return;
+    const currentGame = `${allOptions[playerInput]} vs ${allOptions[cpuInput]}`;
+    
+        switch ((allOptions.length + cpuInput - playerInput) % allOptions.length) {
+            case 0:
+                alert(`${currentGame} is a tie!!`);
+                break;
+            case 2:
+            case 4:
+                alert(`${currentGame} - you win!`);
+                playerScore++;
+                break;
+            default:
+                alert(`${currentGame} - you lose!`);
+                cpuScore++;
+                break;
+        }
     }
 
-    // If player choses rock
-    if (playerInput === 'Rock') {
-        if (cpuInput === 'Scissors') {
-            alert(`${currentGame} - you win!`);
-            playerScore++;
-    } else if (cpuInput === 'Lizard') {
-            alert(`${currentGame} - you win!`);
-            playerScore++;
-    } else {
-            alert(`${currentGame} - you lose!`);
-            cpuScore++;
-    }
-    }
-    // If player choses  paper
-    else if (playerInput === 'Paper') {
-        if (cpuInput === 'Rock') {
-            alert(`${currentGame} - you win!`);
-            playerScore++;
-    } else if (cpuInput === 'Spock') {
-            alert(`${currentGame} - you win!`);
-            playerScore++;
-    } else {
-            alert(`${currentGame} - you lose!`);
-            cpuScore++;
-    }
-    }
-    // If player choses scissors
-    else if (playerInput === 'Scissors') {
-        if (cpuInput === 'Paper') {
-            alert(`${currentGame} - you win!`);
-            playerScore++;
-    } else if (cpuInput === 'Lizard') {
-            alert(`${currentGame} - you win!`);
-            playerScore++;
-    } else {
-            alert(`${currentGame} - you lose!`);
-            cpuScore++;
-    }
-    }
-    // If player choses lizard
-    else if (playerInput === 'Lizard') {
-        if (cpuInput === 'Paper') {
-            alert(`${currentGame} - you win!`);
-            playerScore++;
-    } else if (cpuInput === 'Spock') {
-            alert(`${currentGame} - you win!`);
-            playerScore++;
-    } else {
-            alert(`${currentGame} - you lose!`);
-            cpuScore++;
-    }
-    } 
-    // If player choses spock
-    else if (playerInput === 'Spock') {
-        if (cpuInput === 'Rock') {
-            alert(`${currentGame} - you win!`);
-            playerScore++;
-    } else if (cpuInput === 'Scissors') {
-            alert(`${currentGame} - you win!`);
-            playerScore++;
-    } else {
-            alert(`${currentGame} - you lose!`);
-            cpuScore++;
-    }
-    }
-}
-
+// Function to update score
 function updateScore() {
     document.getElementById("player-score").textContent = playerScore;
     document.getElementById("cpu-score").textContent = cpuScore;
