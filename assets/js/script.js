9342f1dc0f4bb7cab0f06c18498c5abb13ee61f2
Christian Bergane <107763472+CBergane@users.noreply.github.com>
2022-09-07 21:38:18 +0200

    fixed so divs get display none and not from pressing the buttons of choices and a return button, cpu and player score reset when game starts again but not untill you click a choice

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,37 @@
 const option = document.querySelectorAll('.option');
 const allOptions = ['Rock', 'Paper', 'Scissors', 'Spock', 'Lizard'];
+const conditions = document.getElementById('conditions');
 let playerScore = 0;
 let cpuScore = 0;
 
+// utility for animation on bg
 const animation = document.getElementsByClassName('bg-animation')[0];
 const block = document.getElementsByClassName('block');
 
-
+// hides and shows landing page and game page 
 let startGames = document.querySelector('.score');
 let isShow = true;
-// starGame.style.display = 'none';
-//     function startEasyGame() {
-//         if (isShow) {
-//         starGame.style.display = 'block';
-//         isShow = false;
-//         }else{
-//             starGame.style.display = 'none';
-//         isShow = true;
-//         }
-// };
-function startGame(){
+
+function startGame(wins){
     isShow = !isShow;
     startGames.classList.toggle('hidden')
+    document.getElementById( 'start-game' ).style.display = 'none';
+    numWins = wins;
+    cpuWins = 10;
+    conditions.textContent = `If you get ${numWins} wins, you will win!`;
+    playerScore = 0;
+    cpuScore = 0;
 };
+
+let returnMenus = document.querySelector('.score');
+let isReShow = true;
+
+// Return to starting menu
+function returnMenu() {
+    isReShow = !isReShow;
+    returnMenus.classList.add('hidden', isReShow);
+    document.getElementById( 'start-game' ).style.display = 'block';
+}
 
 // add event listener on click for buttons options
 option.forEach((option) => {
@@ -70,14 +79,15 @@ function updateScore() {
 }
 
 function ifWinner() {
-    if (playerScore === 10 || cpuScore === 10) {
-        const win =
-            playerScore === 10 ? "You beat the game! Well done! Luck is on your side!"
-             :"Better luck next time! Cpu wins!";
-        outcome.innerHTML = (win);
-        return true;
+    if (playerScore === numWins) {
+        outcome.innerHTML = "You beat the game! Well done! Luck is on your side!";
+            return true;
+    } else if (cpuScore === cpuWins) {
+        outcome.innerHTML = "Better luck next time! Cpu wins!";
+            return true;
+    } else {
+        return false;
     }
-    return false;
 }
 
 // add collapse function on rules button
@@ -90,9 +100,8 @@ function closeRules(){
     rules.classList.remove("open-rules");
 };
 
-// for the animated bg
-
+// for the animated bg, adds divs to the html as a bg
 for (var i = 1; i < 400; i++){
     animation.innerHTML += "<div class='block'></div>";
-    block[i].style.animationDelay = `${i*0.05}s`;
+    block[i].style.animationDelay = `${i*0.03}s`;
 };
